b14fa20b1a5d7e3668e6c6115a7216f541bf01ba
dismiro <117271289+dismiro@users.noreply.github.com>
2022-12-01 09:56:55 +0000

    fix typos in prima-game

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -6,7 +6,7 @@ const descriptionGame = 'Answer "yes" if given number is prime. Otherwise answer
 function isPrime(num) {
   if (num <= 1) return false;
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    if (num % i == 0) return false;
+    if (num % i === 0) return false;
   }
   return true;
 }
@@ -15,9 +15,9 @@ function getGameData() {
   // setting number limits
   const lowerBoudnInt = 1;
   const upperBoundInt = 30;
-  
+
   const question = getRandomInt(lowerBoudnInt, upperBoundInt);
-  const answer = isPrime(question)?'yes': 'no';
+  const answer = isPrime(question) ? 'yes' : 'no';
   return [question, answer];
 }
 export default function evenGame() {
